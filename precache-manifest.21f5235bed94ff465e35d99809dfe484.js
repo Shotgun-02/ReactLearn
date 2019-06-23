20f03c580eeada086e5d8e7ed97ed19349ed5523
Shotgun-02 <tkamble02@gmail.com>
2019-06-24 02:15:25 +0530

    Updates

---
+++ precache-manifest.21f5235bed94ff465e35d99809dfe484.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb9d087126a8e9d064bfaa83de76a4cf",
+    "url": "/ReactLearn/index.html"
+  },
+  {
+    "revision": "e8fe5106e3116cc823d5",
+    "url": "/ReactLearn/static/css/main.84c3dea6.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/ReactLearn/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "e8fe5106e3116cc823d5",
+    "url": "/ReactLearn/static/js/main.16f28e0e.chunk.js"
+  },
+  {
+    "revision": "b1b2ccccf5b295a46063",
+    "url": "/ReactLearn/static/js/runtime~main.f0db4aec.js"
+  }
+]);
